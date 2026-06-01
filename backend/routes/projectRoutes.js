3c803b018ff9bb69f9311009a017a770fb608128
Tracey Roberts <traceynicole71@yahoo.com>
2026-06-01 14:56:04 -0400

    projectRoutesBE

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,30 @@
+
+//RESTFUL aPI routing
+const express = require('express');
+const router = express.Router();
+
+const {
+    getProjects,
+    getProjectById,
+    createProject,
+    updateProject,
+    deleteProject
+} = require('../controllers/projectController');
+
+//Import authMiddleware
+const { protect } = require('../middleware/authMiddleware');
+//Secure route with mddielware
+router.use(protect);
+
+//Base routing path map to REST specifications
+router.route('/')
+.get(getProjects)
+.post(createProject);
+
+//Parameter paths
+router.route('/:id')
+.get(getProjectById)
+.put(updateProject)
+.delete(deleteProject);
+
+module.export = router;

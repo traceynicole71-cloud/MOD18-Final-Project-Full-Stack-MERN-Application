9e0ec96c47f25bd8e89f99a416e16455c68f1d19
Tracey Roberts <traceynicole71@yahoo.com>
2026-06-02 11:54:06 -0400

    userRoutesBE

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser } = require('../controllers.userController');
+
+//REST structure for account creation and entry access
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+module.exports = router;
